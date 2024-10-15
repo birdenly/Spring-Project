c3c64710a675eb64bbeef0719a518af1c5a083db
birdenly <jomacaldas@hotmail.com.br>
2024-10-15 00:47:32 -0300

    Order entity, oneToMany - manyToOne

--- src/main/java/com/firstspring/project/ententies/User.java
+++ src/main/java/com/firstspring/project/ententies/User.java
@@ -1,11 +1,14 @@
 package com.firstspring.project.ententies;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity//Indicate that it is a ententy for the DB
@@ -20,6 +23,10 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")//atribbute name on the order class
+    private List<Order> orders =  new ArrayList<>();
+
     public User() {
     }
     public User(Long id, String name, String email, String phone, String password) {
@@ -46,6 +53,9 @@ public class User implements Serializable{
     }
     public String getEmail() {
         return email;
+    }
+    public List<Order> getOrders() {
+        return orders;
     }
     public void setEmail(String email) {
         this.email = email;
