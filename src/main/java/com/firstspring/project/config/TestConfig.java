7f3018090afb91f64849922f686010998dd73dfe
birdenly <jomacaldas@hotmail.com.br>
2024-10-20 01:21:49 -0300

    Payment, one-to-one association

--- src/main/java/com/firstspring/project/config/TestConfig.java
+++ src/main/java/com/firstspring/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.firstspring.project.entities.Category;
 import com.firstspring.project.entities.Order;
 import com.firstspring.project.entities.OrderItem;
+import com.firstspring.project.entities.Payment;
 import com.firstspring.project.entities.Product;
 import com.firstspring.project.entities.User;
 import com.firstspring.project.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        //to save onetoone relations, use the parent repository
+        Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
 
     }
 
