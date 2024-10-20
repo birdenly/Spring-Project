e96892f8e39ec6409d87d9a4c9bed1fef887f679
birdenly <jomacaldas@hotmail.com.br>
2024-10-20 02:07:11 -0300

    user update

--- src/main/java/com/firstspring/project/services/UserService.java
+++ src/main/java/com/firstspring/project/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
     public void delete(Long id){
         repository.deleteById(id);
     }
+
+    public User update(Long id, User obj){
+        User entity = repository.getReferenceById(id); //efficient way to prepare an entity, doesnt go to the DB
+        updateData(entity,obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
